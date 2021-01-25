75e27590049839f840ed8e5937c1cf6d11d51b97
aam198 <aam198@psu.edu>
2021-01-25 13:57:52 -0500

    updated message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Added Auth :D </h1> 
+        <h1>Added AWS  Auth  </h1> 
         <a
           className="App-link"
           href="https://reactjs.org"
